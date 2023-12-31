7a3ad681e4a7257f8cd55c5bd0bdbd2c903a969b
Kaur Ruukel <113232105+kaurruukel@users.noreply.github.com>
2023-12-31 13:32:36 +0200

    database env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { SongsModule } from './songs/songs.module';
             port: parseInt(process.env.POSTGRES_PORT), // the port of your database
             username: process.env.POSTGRES_USERNAME, // the username to connect to your database
             password: process.env.POSTGRES_PASSWORD, // the password to connect to your database
-            database: process.env.POSTGRES_NAME, // the name of your database
+            database: process.env.POSTGRES_DB, // the name of your database
             entities: [__dirname + '/**/*.entity{.ts,.js}'], // the path to your entities
             synchronize: false, // whether to automatically create the database schema on every application launch
         }),
